7288366e14ca517c2ba3cbac13bbb3c1ec83871f
PradumnO9 <pradumn2999@gmail.com>
2026-03-21 10:00:31 +0530

    changed

--- FRONTEND/src/App.tsx
+++ FRONTEND/src/App.tsx
@@ -1,7 +1,4 @@
-import { useState } from "react";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <main className="py-10 bg-gray-50 h-screen">
       <h1 className="text-4xl font-bold">Hello Restaurant</h1>
