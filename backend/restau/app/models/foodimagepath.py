e3baed864d9cd5f393d2be6d862f5aef940a67c1
gokumuivkd <dhokiavishal@gmail.com>
2026-05-05 15:27:32 +0530

    commit

--- backend/restau/app/models/foodimagepath.py
+++
@@ -1,8 +0,0 @@
-from sqlmodel import Field, JSON, Column, Relationship 
-from typing import Optional, List 
-from .sqllmodel import SQLModel
-
-
-class FoodImagesPath(SQLModel,table=True): 
-    FoodImagesPathId : Optional[int] = Field(default= None, primary_key=True)
-    FoodImagesPathName: str = Field(default= None)
